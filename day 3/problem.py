cd6e376f17c483e408d298444c9b9a1a251344c6
Jake Cratty <jcratty.1234@gmail.com>
2021-12-02 23:31:57 -0800

    Day 3 updated
    Updated some code, added comments

--- day 3/problem.py
+++ day 3/problem.py
@@ -6,15 +6,7 @@ with open("input.txt") as input_file:
 	diagnosticReport = [str(line).split() for line in input_file.readlines()]
 
 
-#define functions used in both problems 1 and 2
-def getGammaAndEpsilon(commonBitList):
-	gamma_rate = ""
-	epsilon_rate = ""
-	for bit in bitCountPerColumn:
-		gamma_rate += str(bit)
-		epsilon_rate += str((bit + 1) % 2)
-	return gamma_rate, epsilon_rate
-
+#functions used in both problems 1 and 2
 def getCommonBitPerColumn(bitStringList, index):
 	#iterate over each bitstring in bitStringList, get most common bit in each column
 	bitFrequency = [0, 0]
@@ -28,18 +20,29 @@ def getCommonBitPerColumn(bitStringList, index):
 	return mostCommonBit
 
 #problem 1
+def getGammaAndEpsilon(commonBitList): #build gamma and epsilon rates based on the common bit list
+	gamma_rate = ""
+	epsilon_rate = ""
+	for bit in commonBitList:
+		gamma_rate += str(bit)
+		epsilon_rate += str((bit + 1) % 2) #flip bits for epsilon
+	return gamma_rate, epsilon_rate
+
 rateLength = len(diagnosticReport[0][0]) #length of each bit string
 
-bitCountPerColumn = [[0] for _ in range(rateLength)]
-for i in range(len(bitCountPerColumn)):
-	bitCountPerColumn[i] = getCommonBitPerColumn(diagnosticReport, i)
+bitCountPerColumn = [[0] for _ in range(rateLength)] #a list of the most common bits per column
+for columnIndex in range(len(bitCountPerColumn)):
+	bitCountPerColumn[columnIndex] = getCommonBitPerColumn(diagnosticReport, columnIndex) #calculate most common bit per column
 gamma_rate, epsilon_rate = getGammaAndEpsilon(bitCountPerColumn)
 
+ #convert bitstring to base 10
 gamma_int = int(gamma_rate, 2)
 epsilon_int = int(epsilon_rate, 2)
 
 print(f"Gamma: {gamma_int}\nEpsilon: {epsilon_int}\nPart 1 Answer: {gamma_int * epsilon_int}\n")
 
+
+#----------------------------------------------------------------------------------------------------------------
 #problem 2
 def refineList(bitStringList, criteria):
 	for i in range(len(bitStringList)-1, -1, -1):
