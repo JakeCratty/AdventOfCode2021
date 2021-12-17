d4c6bcaf679b11425ff48646eeffe2a8e0d14c44
Jake Cratty <jcratty.1234@gmail.com>
2021-12-17 11:17:28 -0800

    Day 16 Complete
    Finished Day 16. Overall it was actually pretty easy, I just got stuck because I misinterpreted the instructions for part 1. Part 2 was simple to integrate. Still mad I accidentally deleted Day 15 part 1 in the process.

--- day 16/problem2.py
+++ day 16/problem2.py
@@ -44,54 +44,75 @@ def extractLiteral(literal):
 
 	pass
 
+def applyOperator(id, valueList):
+	if(id == 0):
+		result = 0
+		for value in valueList:
+			result += value
+	elif(id == 1):
+		result = 1
+		for value in valueList:
+			result *= value
+	elif(id == 2):
+		result = -1
+		for value in valueList:
+			if value < result or result == -1:
+				result = value
+	elif(id == 3):
+		result = -1
+		for value in valueList:
+			if value > result: result = value
+	elif(id == 5):
+		result = (0, 1)[valueList[0] > valueList[1]]
+	elif(id == 6):
+		result = (0, 1)[valueList[0] < valueList[1]]
+	elif(id == 7):
+		result = (0, 1)[valueList[0] == valueList[1]]
+	return result
+
 def parsePacket(binary, pt, packet_level):
-	print(f"--New Packet Detected on layer {Fore.RED}{packet_level}{Style.RESET_ALL}.")
 	packetLength = 6 #tracks this packets total length to be returned at the end
 	versionNumber = int(binary[pt:pt+3], 2)
-	versionTotal = versionNumber
 	packetID = int(binary[pt+3:pt+6], 2)
 	pt += 6
-	print(f"\tVersion Number: {Fore.GREEN}{versionNumber}{Style.RESET_ALL}, Packet ID: {Fore.YELLOW}{packetID}{Style.RESET_ALL}")
 	
 	if(packetID == 4): #literal value
 		literal = binary[pt:]
-		literal, length = extractLiteral(literal)
+		result, length = extractLiteral(literal)
 		pt += length
-		print(f"\tPacket value: {literal}")
 	else: #operator
 		lengthTypeID = int(binary[pt:pt+1], 2)
 		pt += 1
-		print(f"\tLength Type ID: {lengthTypeID}")
+		subpacket_values = []
 		if(lengthTypeID == 0):
 			lengthOfSubPackets = int(binary[pt:pt+15], 2)
-			print(f"\tLength of subpackets: {lengthOfSubPackets}")
 			pt += 15
 			index = pt
 			while(index < pt + lengthOfSubPackets):
-				versionNumber, index = parsePacket(binary, index, packet_level + 1)
-				versionTotal += versionNumber
+				result, index = parsePacket(binary, index, packet_level + 1)
+				subpacket_values.append(result)
 			pt = index
 		else:
 			numberOfSubPackets = int(binary[pt:pt+11], 2)
-			print(f"\tNumber of sub packets: {numberOfSubPackets}")
 			pt += 11
 			index = pt
 			packets = 0
 			while(packets < numberOfSubPackets):
-				versionNumber, index = parsePacket(binary, index, packet_level + 1)
-				versionTotal += versionNumber
+				result, index = parsePacket(binary, index, packet_level + 1)
+				subpacket_values.append(result)
 				packets += 1
 			pt = index
+		result = applyOperator(packetID, subpacket_values)
 
 	if(packet_level == 0):
-		return versionTotal
+		return result
 	else:
-		return versionTotal, pt
+		return result, pt
 
 if __name__ == "__main__":
 	init()
 	with open("input.txt") as input_file:
 		hexString = input_file.readline()
 	binaryString = hexToBinary(hexString)
-	totalVersionNumber = parsePacket(binaryString, 0, 0)
-	print(f"Part 1 Answer: {totalVersionNumber}")
+	total = parsePacket(binaryString, 0, 0)
+	print(f"Part 2 Answer: {total}")
